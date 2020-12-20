afafd4adf46bc60f10073e8456bf0521fd3d7c06
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-20 17:35:52 -0600

    render's the photos and title of movies

--- src/Search/SearchList.js
+++ src/Search/SearchList.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const SearchList =({movieResults}) =>{
-    return <div></div>
+
+    function showMovieList(){
+        return movieResults.map((item, i)=>{
+            return (
+                <li key ={i}>
+                    <img src ={item.Poster} alt ="some good movie"/>
+                    {item.Title}
+                </li>
+            )
+        })
+    }
+    return <ul><div>{showMovieList()}</div></ul>
 };
 
 export default SearchList;
