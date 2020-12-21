c3a03d4222ea7b0d9c7c7bf7e084e27e3051f80d
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 12:22:32 -0600

    implimenting context

--- src/context/MovieContext.js
+++ src/context/MovieContext.js
@@ -1,9 +1,3 @@
 import React from 'react'
 
-export const MovieContext = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+export const MovieContext = React.createContext({})
