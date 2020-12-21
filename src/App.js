6c62caa007e1418ad3ad4f77c1afc6f9b1e3e9a6
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 02:20:57 -0600

    adding movie to app.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
 import {useState} from 'react'
 import './App.css';
 import Search from "./Search/Search";
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Movie from "./context/Movie/Movie";
 
-
-function App (){
-  return(
-    <Router>
-      <Route to ="/" component={AppNav}/>
-    </Router>
-
-  )
-}
 
 function AppNav() {
-
-
+  
+  
   
   const [searchValue, setSearchValue] = useState("");
-
+  
   //passes the values down
-//null in the beginning
+  //null in the beginning
   const  [movieResults, setMovieResults] = useState([]);
 
+  const [isFetching, setIsFetching] = useState(false);
+
+  
   async function fetchMovieListApi(inputValue){
     setSearchValue(inputValue);
 
-    const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_OMBD_API;
+    if(!searchValue){
+      setIsFetching(false);
+      return;
 
+    }
+    
+    const MOVIE_API_KEY = process.env.REACT_APP_MOVIE_OMBD_API;
+    
     try{
       const response = await fetch(`http://omdbapi.com/?apikey=${MOVIE_API_KEY}&s=${inputValue}`)
-   
+      
       const data = await response.json();
 
-      setMovieResults(data.Search|| [])
+      if(!data.Error){
+        setIsFetching(true);
+        setMovieResults(data.Search);
 
-
-
-
+      }
+      
+      // setMovieResults(data.Search|| [])
+      
+      
+      
+      
     }catch(e){
-
+      
     } 
   }
   return (
@@ -49,11 +56,27 @@ function AppNav() {
       searchValue={searchValue} 
       fetchMovieListApi ={fetchMovieListApi} 
       movieResults={movieResults}
+      isFetching ={isFetching}
       />
 
      
     </div>
   );
+}
+function App (){
+  return(
+<Router>
+<Switch>   
+      <Route exact path to ="/" component={AppNav}/>
+      <Route exact path to ="/:movieTitle" component={Movie}/>
+      <Route render={()=><h1>Not Found</h1>}/>
+</Switch>
+
+    </Router>
+
+
+  )
+
 }
 
 export default App;
