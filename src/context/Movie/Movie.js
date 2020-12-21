4b13cb3da59cee608e26e734692bda643abb253d
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 10:32:12 -0600

    adding asnyc function to movie.js

--- src/context/Movie/Movie.js
+++ src/context/Movie/Movie.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {useLocation} from 'react-router-dom'
 import MovieDetails from "./MovieDetails";
+import queryString from 'query-string';
+import { useLocation } from 'react-router-dom';
+
 
 const Movie = () => {
+
+    const {search} =useLocation();
+    const [movieDetail, setMovieDetail] = useState(null);
+    const title = queryString.parse(search);
+
+    useEffect(()=>{
+
+    },[])
+
+
     return (
         <div>
             <MovieDetails />
