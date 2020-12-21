7324f6bba179dec23bcdda688ab4cfef565e12d2
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 03:43:18 -0600

    update

--- src/Search/SearchList.js
+++ src/Search/SearchList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './Search.css';
 
 const SearchList =({movieResults}) =>{
@@ -8,7 +9,10 @@ const SearchList =({movieResults}) =>{
             return (
                 <li key ={i}>
                     <img src ={item.Poster} alt ="some good movie"/>
+                    <Link to ={`${item.Title}`}>
                     {item.Title}
+                    
+                    </Link>
                 </li>
             )
         })
